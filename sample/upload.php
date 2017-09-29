1affdf6b3a09517673c656ed8cbe098dd5f92dd1
Cres Jie <cresjie@gmail.com>
2017-09-29 17:53:32 +0800

    profile uploader

--- sample/upload.php
+++ sample/upload.php
@@ -1,1 +1,25 @@
 <?php
+
+include('vendor/autoload.php');
+
+use Intervention\Image\ImageManagerStatic as Image;
+
+header('Content-type:application/json');
+
+// configure with favored image driver (gd by default)
+
+if( isset($_FILES['image']) ) {
+
+	Image::configure(array('driver' => 'imagick'));
+
+	$img = Image::make($_FILES['image']['tmp_name']);
+
+	if( isset($_POST['x']) && isset($_POST['y']) && isset($_POST['width']) && isset($_POST['height']) ) {
+		$img->crop($_POST['width'], $_POST['height'], $_POST['x'], $_POST['y']);
+	}
+
+	echo json_encode(['success' => true, 'data' => (string) $img->encode('data-url') ]);
+	exit;
+}
+
+echo json_encode(['success' => false, 'error_msg' => 'image is required']);
