6486506d4473098e7bf154721126e4ff8f9c9dfb
Cres Jie <cresjie@gmail.com>
2017-09-29 17:53:32 +0800

    profile uploader

--- js/app.js
+++ js/app.js
@@ -34,5 +34,57 @@ $(document).ready(function(){
 		}
 	});
 
+
+
+	/**
+	 * profile image sample
+	 */
+	var readyAndShowed = false;
+
+	function initCropper(){
+		if(readyAndShowed) {
+			$('.profile-preview').cropper({
+				aspectRatio: 1,
+			})
+		}
+
+		readyAndShowed = true;
+	}
+
+	var profileUploader = new FileWizard('#profile-uploader',{
+		url: 'upload.php',
+		multipleFiles: false,
+		fileAdded: function(file){
+			$('#profile-modal').modal('show');
+
+			var filereader = new FileReader;
+
+			filereader.onload = function(e) {
+
+				$('#profile-modal .profile-preview').attr('src', e.target.result);
+				initCropper();
+				
+			}
+
+
+			filereader.readAsDataURL(file);
+		},
+		progress: function(percent){
+			
+		}
+	});
+
+	$('#profile-modal').on('shown.bs.modal', initCropper)
+		.on('hide.bs.modal', function(){
+			$('.profile-preview').cropper('destroy');
+			$('#profile-modal .progress').hide().width(0);
+			readyAndShowed = false;
+		});
+
+	$('#submit-profile').on('click', function(){
+		$('#profile-modal .progress').show();
+	})
+
+	
 	
 })
